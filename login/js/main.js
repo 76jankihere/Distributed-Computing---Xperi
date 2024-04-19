73356f73f95ad07616289cde7f592be935514d0b
76jankihere <108918275+76jankihere@users.noreply.github.com>
2024-04-19 08:13:37 -0400

    Update main.js

--- login/js/main.js
+++ login/js/main.js
@@ -12,7 +12,6 @@ $(window).on("load", function() {
             $('.error-message').remove();
             $('<small class="error-message">Please fill the field.</small>').insertAfter(inputField);
         }
-
     });
     
     $('.btn-tab-next').on('click',function(e){
